b8b1e4a6cff357f856670cc940c48e2a695cc9ad
Antonin <anto10800@gmail.com>
2024-03-29 17:29:08 +0100

    forums/id/sujets

--- pages/forums/[idForums]/index.vue
+++ pages/forums/[idForums]/index.vue
@@ -9,18 +9,15 @@ export default {
   data() {
     return {
       sujets: null,
-      id: this.$route.params.id,
+      id: this.$route.params.idForums,
       page: 1,
     };
   },
   async mounted() {
     try {
-      this.sujets = await $fetch(`/api/forums/${this.id}/sujets?page=${this.page}`, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      console.log(this.sujets.data)
+      this.sujets = await $fetch(`/api/forums/${this.id}/sujets?page=${this.page}`);
+    this.sujets = this.sujets.data
+      console.log(this.sujets)
     } catch (error) {
       console.error('Erreur lors de la récupération des forums :', error);
     }
@@ -33,18 +30,22 @@ export default {
   <v-container>
     <Header />
     <v-row>
-      <v-col>
-        <v-card>
+      <v-col class="justify-center">
+        <v-card class="mx-auto my-2"
+                max-width="1000">
           <v-card-title>
-            <h1>Forums</h1>
+            <h1 class="text-center mb-8">Forums</h1>
           </v-card-title>
           <v-card-text>
-            <v-list>
+            <v-list class="text-center">
 
-                <v-list-item v-for="sujet in sujets.data" :key="sujet.id">
+                <v-list-item v-for="sujet in sujets" :key="sujet.id" class="mb-4">
+                  <nuxt-link :to="`/forums/${sujet.forum_id}/sujets/${sujet.id}`" class="text-decoration-none text-h6">
                   <v-list-item-title>{{ sujet.title }}</v-list-item-title>
-                  <v-list-item-subtitle>{{ sujet.login }}</v-list-item-subtitle>
-
+                  <v-list-item-subtitle>Auteur : {{ sujet.login }}</v-list-item-subtitle>
+                    <v-list-item-title>Date de parution : ({{ sujet.date }})</v-list-item-title>
+                    <v-list-item-title>Dernier message de : </v-list-item-title>
+                  </nuxt-link>
                 </v-list-item>
 
             </v-list>
