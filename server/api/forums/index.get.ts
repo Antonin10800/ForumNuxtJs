a0d474d2a8038ca104c2f6f2775faf5e107f3a5f
Stanislas JEANGEY <stanislas.jeangey@orange.fr>
2024-03-29 14:57:10 +0100

    update de la route /api/forums pour ajouter un count des sujets

--- server/api/forums/index.get.ts
+++ server/api/forums/index.get.ts
@@ -1,7 +1,7 @@
 import db from '~/server/sql'
 
 export default defineEventHandler(async (event) => {
-    let query = `SELECT * FROM forums`
+    let query = `SELECT forums.*, Count(sujets.id) AS nbSujets FROM forums LEFT JOIN sujets ON forums.id = sujets.forum_id GROUP BY(forums.id)`
     let [rows, _] = await db.execute(query)
     return rows
 });
