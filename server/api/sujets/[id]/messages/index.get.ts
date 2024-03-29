62e2f77c8418f41b0d42d2276b46e0a0d41ef70b
Stanislas JEANGEY <stanislas.jeangey@orange.fr>
2024-03-29 17:05:56 +0100

    liste des messages et sujets triés par date et avec page

--- server/api/sujets/[id]/messages/index.get.ts
+++ server/api/sujets/[id]/messages/index.get.ts
@@ -2,23 +2,49 @@ import {defineWrappedResponseHandler} from "~/server/utils/mysql";
 
 export default defineWrappedResponseHandler(async (event) => {
     const db = event.context.mysql
-  if (event.context.params){
-      if(!event.context.params.id){
-          setResponseStatus(event, 400)
-          return{
+    if (event.context.params) {
+        if (!event.context.params.id || event.context.params.id === '') {
+            setResponseStatus(event, 400)
+            return {
                 error: "id not found"
-          }
-      } else {
-            let idSujet = event.context.params.id
-            let query = "SELECT * FROM messages WHERE sujet_id = ?"
-            let [rows, _] = await db.execute(query, [idSujet])
+            }
+        } else {
+            let NbParPage = 5
+            let page = parseInt(<string>getQuery(event).page) || 1
+            let id = event.context.params.id
+            let IDpage = (page-1) * NbParPage
+
+            let queryNombreMessages = "SELECT COUNT(*) as nbMessages FROM messages WHERE sujet_id = ?"
+            let [rowsNombreMessages, __] = await db.execute(queryNombreMessages, [id])
+
+            let pageMax = Math.ceil(rowsNombreMessages[0].nbMessages / NbParPage) - 1
+            if (page > pageMax) {
+                setResponseStatus(event, 404)
+                return {
+                    error: "page not found"
+                }
+            }
+            let query = "SELECT messages.id, messages.content, messages.date, messages.sujet_id, users.login FROM messages LEFT JOIN users ON users.id = messages.author_id WHERE sujet_id = ? ORDER BY date LIMIT ? OFFSET ?"
+            let [rows, _] = await db.execute(query, [id, NbParPage, IDpage])
+
             if (Array.isArray(rows) && rows.length === 0) {
                 setResponseStatus(event, 404)
                 return {
-                    error: `Sujets not found : ${idSujet}`
+                    error: `Sujets not found : ${id}`
                 }
             }
-            return rows
-      }
-  }
+            let nombreSujets = rowsNombreMessages[0].nbMessages
+            return {
+                page,
+                pageMax,
+                nombreSujets,
+                data: rows
+            }
+        }
+    } else {
+        setResponseStatus(event, 400)
+        return {
+            error: "id not found"
+        }
+    }
 })
