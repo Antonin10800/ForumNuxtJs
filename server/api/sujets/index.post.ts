146ad2fa456985f1159e91edadb5de3345208184
Stanislas JEANGEY <stanislas.jeangey@orange.fr>
2024-03-30 09:32:50 +0100

    update création de sujet

--- server/api/sujets/index.post.ts
+++ server/api/sujets/index.post.ts
@@ -3,12 +3,20 @@ import {defineWrappedResponseHandler} from "~/server/utils/mysql";
 export default defineWrappedResponseHandler(async (event) => {
     const db = event.context.mysql
     let body = await readBody(event);
-    if (!body.title || !body.author || !body.forum || body.title === "" || body.author === "" || body.forum === "") {
+    if (!body.title || !body.author || !body.forum || body.title === "" || isNaN(body.author) || isNaN(body.forum)) {
         setResponseStatus(event, 400)
         return {
             error: "Information missing"
         }
     } else {
+        let [author] = await db.execute('SELECT * FROM users WHERE id = ?', [body.author])
+        let [forum] = await db.execute('SELECT * FROM forums WHERE id = ?', [body.forum])
+        if (author.length === 0 || forum.length === 0) {
+            setResponseStatus(event, 404)
+            return {
+                error: "Author or forum not found"
+            }
+        }
         let [result] = await db.execute(
             "INSERT INTO sujets (title, author_id, forum_id, date) VALUES (?, ?, ?, NOW())", [body.title, body.author, body.forum]
         )
